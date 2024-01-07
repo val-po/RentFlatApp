d0de198db9ee36022503a0b27f0b3a596efb575e
val-po <valentine.potapov@gmail.com>
2024-01-07 11:23:08 +0200

    Initial Commit

---
+++ RentFlatApp/RentFlatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentFlatAppApp.swift
+//  RentFlatApp
+//
+//  Created by Val Po on 07.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RentFlatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
